9075a43bbf6dd483ba3c13797b4a7894892ef280
Chester Leung <chester.leung@berkeley.edu>
2020-06-05 22:24:18 +0000

    Support party starting RPC server to listen for aggregator, integration into xgboost package

---
+++ demo/rpc/demo.py
@@ -0,0 +1,44 @@
+import sys
+
+rabit_config = json.loads(sys.argv[1])
+
+# Instantiate Federated XGBoost
+fed = xgb.Federated(rabit_config)
+
+# Get number of federating parties
+print(fed.get_num_parties())
+
+# Load training data
+# Ensure that each party's data is in the same location with the same name
+print("Load data")
+dtrain = fed.load_data("/home/ubuntu/federated-xgboost/demo/data/hb_train.csv")
+dval = fed.load_data("/home/ubuntu/federated-xgboost/demo/data/hb_val.csv")
+
+print('setting params')
+# Train a model
+params = {
+        "max_depth": 3, 
+        "min_child_weight": 1.0, 
+        "lambda": 1.0,
+        "tree_method": "hist",
+        "objective": "binary:logistic"
+        }
+
+num_rounds = 20
+print("Training")
+bst = xgb.train(params, dtrain, num_rounds, evals=[(dtrain, "dtrain"), (dval, "dval")])
+
+dtest = fed.load_data("/home/ubuntu/federated-xgboost/demo/data/hb_test.csv")
+
+# Get predictions
+print("Predicting")
+ypred = bst.predict(dtest)
+
+print("The first twenty predictions are: ", ypred[:20])
+
+# Save the model
+bst.save_model("sample_model.model")
+
+# Shutdown
+fed.shutdown()
+
---
+++ python-package/xgboost/remote_server.py
@@ -0,0 +1,122 @@
+'''
+gRPC Server
+'''
+from concurrent import futures
+
+#  import _credentials
+
+from .rpc import fxgb_pb2
+from .rpc import fxgb_pb2_grpc
+import grpc
+
+import ctypes
+import sys
+
+import pandas as pd
+import json
+import subprocess
+
+
+def get_dmlc_vars(env):
+    '''
+    Returns list of strings representing DMLC variables needed for rabit.
+    Parsed in allreduce_base.cc from '<name>=<value>' format.
+    
+    Param:
+        env - Env protobuf
+    
+    Return:
+        list containing DMLC variables
+    '''
+    temp = [
+        'DMLC_TRACKER_URI=' + env.DMLC_TRACKER_URI,
+        'DMLC_TRACKER_PORT=' + str(env.DMLC_TRACKER_PORT),
+        'DMLC_ROLE=' + env.DMLC_ROLE,
+        'DMLC_NODE_HOST=' + env.DMLC_NODE_HOST,
+        'DMLC_NUM_WORKER=' + str(env.DMLC_NUM_WORKER),
+        'DMLC_NUM_SERVER=' + str(env.DMLC_NUM_SERVER),
+    ]
+    # Python strings are unicode, but C strings are bytes, so we must convert to bytes.
+    return [bytes(s, 'utf-8') for s in temp]
+
+
+class FederatedXGBoostServicer():
+    ''' gRPC servicer class which implements worker machine RPCs API. '''
+
+    def __init__(self):
+        self.dmlc_vars = None
+
+    def Init(self, request, context):
+        '''
+        Initializes rabit and environment variables.
+        When worker receives this RPC, it can accept or reject the federated training session.
+
+        Params:
+            init_request - InitRequest proto containing DMLC variables to set up node communication with tracker
+            context - RPC context. Contains metadata about the connection
+
+        Return:
+            WorkerResponse proto (confirmation of initializatison success or failure).
+        '''
+        print('Request from aggregator [%s] to start federated training session:' % context.peer())
+        accept_job = None
+        while accept_job not in {'Y', 'N'}:
+            print("Please enter 'Y' to confirm or 'N' to reject.")
+            accept_job = input("Join session? [Y/N]: ")
+        if accept_job == 'Y':
+            self.dmlc_vars = get_dmlc_vars(request.dmlc_vars)
+            return fxgb_pb2.WorkerResponse(success=True)
+        else:
+            return fxgb_pb2.WorkerResponse(success=False)
+
+    def Train(self, request, context):
+        '''
+        Starts distributed training.
+
+        Params:
+            train_request - TrainRequest proto containing XGBoost parameters for training
+            context - RPC context containing metadata about the connection
+
+        Return:
+            WorkerResponse proto (confirmation of training success or failure).
+        '''
+        try:
+            print('Request from aggregator [%s] to start federated training session:' % context.peer())
+            path_to_script = request.path
+            dmlc_vars_as_string = json.dumps(self.dmlc_vars)
+            process = subprocess.Popen(["python3", path_to_script, dmlc_vars_as_string], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+            # Real time output of process
+            while True:
+                output = process.stdout.readline()
+                if output == '' and process.poll() is not None:
+                    break
+                if output:
+                    print(output.strip())
+            rc = process.poll()
+
+            if rc == 0:
+                return fxgb_pb2.WorkerResponse(success=True)
+            else:
+                return fxgb_pb2.WorkerResponse(success=False)
+        except:
+            return fxgb_pb2.WorkerResponse(success=False)
+
+
+# Start gRPC server listening on port 'port'
+def listen(port):
+    """
+    Params
+    ------
+    port : int
+        Port to listen for aggregator
+    """
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    fxgb_pb2_grpc.add_FXGBWorkerServicer_to_server(FederatedXGBoostServicer(), server)
+    #  server_credentials = grpc.ssl_server_credentials(
+        #  ((_credentials.SERVER_CERTIFICATE_KEY, _credentials.SERVER_CERTIFICATE),))
+    #  server.add_secure_port('[::]:' + port, server_credentials)
+    server.add_insecure_port('[::]:' + str(port))
+    print("Starting RPC server on port ", str(port))
+    server.start()
+    server.wait_for_termination()
